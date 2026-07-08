acb010834372bd018524edecc621c295d26b0f6b
JH <jeheon.yu@gmail.com>
2026-07-08 14:40:32 +0900

    engine

--- lib/engine/crowd_engine.dart
+++ lib/engine/crowd_engine.dart
@@ -1,9 +1,24 @@
-import 'models/crowd_engine_result.dart';
+import 'calculators/scale_calculator.dart';
+import 'calculators/acceleration_calculator.dart';
+import 'calculators/broker_spread_calculator.dart';
+import 'calculators/investor_spread_calculator.dart';
+import 'calculators/program_confidence_calculator.dart';
+import 'calculators/crowd_score_calculator.dart';
 
 class CrowdEngine {
-  const CrowdEngine();
+  final ScaleCalculator scaleCalculator;
+  final AccelerationCalculator accelerationCalculator;
+  final BrokerSpreadCalculator brokerSpreadCalculator;
+  final InvestorSpreadCalculator investorSpreadCalculator;
+  final ProgramConfidenceCalculator programConfidenceCalculator;
+  final CrowdScoreCalculator crowdScoreCalculator;
 
-  CrowdEngineResult calculate() {
-    throw UnimplementedError();
-  }
+  const CrowdEngine({
+    required this.scaleCalculator,
+    required this.accelerationCalculator,
+    required this.brokerSpreadCalculator,
+    required this.investorSpreadCalculator,
+    required this.programConfidenceCalculator,
+    required this.crowdScoreCalculator,
+  });
 }
